4f81ba554cf4387125d1725f9ea355d00b3e420b
fima-taf <fimabndt@gmail.com>
2021-02-09 13:36:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id="base">
+    <nav>
+      <div class="left">
+        <div class="nav-title">
+          {{ navTitle }} <span class="nav-subtitle">{{ navSubtitle }}</span>
+        </div>
+      </div>
+      <div class="right">
+        <template v-for="(item, index) in navItems">
+          <a href="#" :key="index">{{ item }}</a>
+        </template>
+      </div>
+    </nav>
+    <h1>{{ title }}</h1>
+    <BlogCard :items="cards" subtitleSlotReturnValue="subtitle">
+      <template slot="subtitle" slot-scope="props">
+        <div :style="'color: ' + props.item.color">
+          {{ props.item.subtitle }}
+          <button @click="props.item.color = 'black'">Change color</button>
+        </div>
+      </template>
+      <template slot="footer-extra" slot-scope="{ item }">
+        <div class="rec" v-if="item.extra">*-{{ item.extra }}-*</div>
+        <br />
+        {{ item.readtime }}
+      </template>
+    </BlogCard>
+  </div>
+</template>
+
+<script>
+import BlogCard from "@/components/BlogCard";
+
+export default {
+  components: {
+    BlogCard,
+  },
+  data: () => ({
+    title: "Fima's Blog",
+    navTitle: "Blogy",
+    navSubtitle: "(Powered by Klingon)",
+    navItems: ["Login", "Updates", "About"],
+    cards: [
+      {
+        title: "My First Blog",
+        subtitle: "Some nice subtitle",
+        color: "pink",
+        content:
+          "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.....",
+        readtime: "1 min read",
+        footer: "Read More",
+        extra: "Recommended",
+      },
+      {
+        title: "My Second Blog",
+        color: "green",
+        subtitle: "Another nice subtitle",
+        content:
+          "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronarised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum....",
+        readtime: "2 min read",
+        footer: "Read More",
+      },
+      {
+        title: "My Third Blog (wow)",
+        content:
+          "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining e....",
+        readtime: "3 min read",
+        footer: "Read More",
+      },
+    ],
+  }),
+  computed: {
+    // cards() {
+    //   return [
+    //     {
+    //       title: "My First Blog",
+    //       subtitle: "Some nice subtitle",
+    //       color: "pink",
+    //       content:
+    //         "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.....",
+    //       readtime: "1 min read",
+    //       footer: "Read More",
+    //     },
+    //     {
+    //       title: "My Second Blog",
+    //       color: "green",
+    //       subtitle: "Another nice subtitle",
+    //       content:
+    //         "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronarised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum....",
+    //       readtime: "2 min read",
+    //       footer: "Read More",
+    //     },
+    //     {
+    //       title: "My Third Blog (wow)",
+    //       content:
+    //         "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining e....",
+    //       readtime: "3 min read",
+    //       footer: "Read More",
+    //     },
+    //   ];
+    // },
+  },
+};
+</script>
+
+<style lang="scss">
+#base {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  text-align: center;
+  font-size: 24px;
+  color: #424242;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    background-color: #B2DFDB;
+    padding: 2%;
+
+    .nav-title {
+      font-size: 1.1em;
+    }
+
+    .nav-subtitle {
+      font-size: 0.7em;
+    }
+
+    a {
+      margin: 0 6px;
+      color: #00695C;
+    }
+  }
+
+  h1 {
+    font-size: 3em;
+  }
+
+  .rec {
+    font-size: 0.5em;
+  }
+}
+</style>
---
+++ src/components/BlogCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="blog-card" v-for="(item, index) in items" :key="index">
+      <div class="header">
+        <div>{{ item.title }}</div>
+        <div class="subtitle">
+          <slot name="subtitle" :item="item"></slot>
+        </div>
+      </div>
+      <div class="body">
+        {{ item.content }}
+      </div>
+      <div class="footer">
+        <button>{{ item.footer }}</button>
+        <slot name="footer-extra" :item="item"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array,
+    subtitleSlotReturnValue: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.blog-card {
+  margin: 50px;
+  border: 1px solid #BDBDBD;
+  border-radius: 50px;
+  padding: 10px;
+
+  &:hover {
+    box-shadow: 0 0 2.2px rgba(0, 0, 0, 0.034), 0 0 5.3px rgba(0, 0, 0, 0.048),
+      0 0 10px rgba(0, 0, 0, 0.06), 0 0 17.9px rgba(0, 0, 0, 0.072),
+      0 0 32px rgba(0, 0, 0, 0.1);
+    transition: box-shadow 100ms;
+  }
+
+  .header {
+    padding: 4%;
+    font-size: 2em;
+
+    .subtitle {
+      font-size: 0.5em;
+      max-height: 10vh;
+      overflow-y: auto;
+    }
+  }
+
+  .body {
+    padding: 4%;
+    font-size: 0.8em;
+  }
+
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    padding: 4%;
+    font-size: 1.2em;
+
+    button {
+      border: 1px solid #00796B;
+      color: #00796B;
+      border-radius: 3px;
+      background-color: transparent;
+      font: inherit;
+      font-size: 0.7em;
+
+      &:focus {
+        outline: none;
+      }
+
+      &:active {
+        background-color: #EEEEEE;
+        transition: background-color 100ms;
+      }
+    }
+  }
+}
+</style>
